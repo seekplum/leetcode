3d3bd77c4b7a780144327b1cc0e60101770337df
seekplum <1131909224m@sina.cn>
2018-05-11 14:11:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# leetcode
+
+## 题库
+[英文官网](https://leetcode.com/problemset/all/)
+
+[中文题库](https://leetcode-cn.com/problemset/all/)
+
+## 源码
+源码目录在 `src` 目录下
+
+## 测试文件
+针对每个源码文件会有对应的测试文件在 `tests` 目录下
+
+## 依赖管理
+通过 `pipenv` 对项目依赖的三方库进行管理
---
+++ tests/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+#=============================================================================
+#  ProjectName: leetcode
+#     FileName: test
+#         Desc: 测试函数
+#       Author: seekplum
+#        Email: 1131909224m@sina.cn
+#     HomePage: seekplum.github.io
+#       Create: 2018-05-10 22:45
+#=============================================================================
+"""
+
